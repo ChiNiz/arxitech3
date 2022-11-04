08d336dd609b3069015de2e63813d253bf9c12ab
ChiNiz <90142585+ChiNiz@users.noreply.github.com>
2022-11-04 18:47:15 +0300

    Add files via upload

---
+++ 3practicac/src/Client.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
+public class Client {
+    private Socket clientSocket;
+    private PrintWriter out;
+    private BufferedReader in;
+
+    public void startConnection(String ip, int port) throws IOException {
+        clientSocket = new Socket(ip, port);
+        out = new PrintWriter(clientSocket.getOutputStream(), true);
+        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+    }
+
+    public void sendMessages() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String message = scanner.nextLine();
+            out.println(message);
+            if("end".equals(message)){
+                break;
+            }
+        }
+    }
+
+    public void getMessages() {
+        new Thread(() -> {
+            while (true) {
+                try {
+                    String response = in.readLine();
+                    System.out.println(response);
+                    if("Соединение разорвано".equals(response)){
+                        stopConnection();
+                        break;
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    public void stopConnection() throws IOException {
+        in.close();
+        out.close();
+        clientSocket.close();
+        System.out.println("Закрыто");
+    }
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Client client = new Client();
+        client.startConnection("localhost", 7462);
+        client.getMessages();
+        client.sendMessages();
+    }
+}
---
+++ 3practicac/src/Server.java
@@ -0,0 +1,103 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Server {
+    private static final StringBuilder inputMessage = new StringBuilder();
+    private ServerSocket serverSocket;
+    static final List<PrintWriter> printWriters = new ArrayList<>();
+
+    public void start(int port) throws IOException {
+        serverSocket = new ServerSocket(port);
+        ExecutorService pool = Executors.newFixedThreadPool(20);
+        mailing();
+        while (true) {
+            System.out.println("Сервер запущен...");
+            pool.execute(new MailingHandler(serverSocket.accept()));
+        }
+    }
+
+    private void mailing() {
+        Timer carousalTimer = new Timer();
+        carousalTimer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                if (inputMessage.length() > 0) {
+                    for (PrintWriter out : printWriters) {
+                        try {
+                            System.out.println(inputMessage);
+                            out.println(inputMessage);
+                        } catch (Exception e) {
+                            printWriters.remove(out);
+                        }
+                    }
+                    inputMessage.setLength(0);
+                }
+            }
+        }, 5000, 5000);
+    }
+
+    public void stop() throws IOException {
+        serverSocket.close();
+    }
+
+
+    private static class MailingHandler extends Thread {
+        private BufferedReader in;
+        private PrintWriter out;
+        private final Socket clientSocket;
+
+        public MailingHandler(Socket socket) {
+            this.clientSocket = socket;
+        }
+
+        public void run() {
+            try {
+                out = new PrintWriter(clientSocket.getOutputStream(), true);
+                in = new BufferedReader(
+                        new InputStreamReader(clientSocket.getInputStream()));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            if (out != null) {
+                printWriters.add(out);
+            }
+
+            while (true) {
+                String mes = "";
+                try {
+                    mes = in.readLine();
+                } catch (IOException e) {
+                    break;
+                }
+                if ("end".equals(mes)) {
+                    out.println("Соединение разорвано");
+                    break;
+                }
+                inputMessage.append(mes).append(" ");
+            }
+            printWriters.remove(out);
+            out.close();
+            try {
+                in.close();
+                clientSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        new Server().start(7462);
+    }
+}
